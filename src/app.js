857272405f3034b29e79b1c9b0f13af5c33bebc7
snyder-io <tushar.whitecollar@gmail.com>
2024-11-21 14:57:13 +0530

    add utils

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express();
+
+app
+    .use(cors({
+        origin: process.env.CORS_ORIGIN,
+        Credential: true
+    }));
+
+app
+    .use(express.json({
+        limit: "16kb"
+    }));
+
+app
+    .use(express.urlencoded({
+        extended: true,
+        limit: "16kb"
+    }));
+
+app
+    .use(express.static("public"));
+
+app
+    .use(cookieParser());
+
+
+
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 require('dotenv').config({ path: './.env' })
 const { connectDB } = require("./db/index");
+const app  = require("./app")
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(
+                `Server is running at port: ${process.env.PORT}`
+            )
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed !!!", error)
+    })
